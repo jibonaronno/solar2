eb7afe6dfc340be5005a89c7d1677ffd07cac151
jibonaronno <tebunia@gmail.com>
2020-08-06 02:53:12 +0600

    UART1 @ PA9 PA10 send/rec with interrupt working

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Solar2' 
+ * Target:  'Solar2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/Solar2/startup_stm32f107xc.d
@@ -0,0 +1,1 @@
+solar2\startup_stm32f107xc.o: startup_stm32f107xc.s
